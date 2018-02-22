87cbfcf6262a75be35f69e79382008eeeed74122
haithamsha <haithamshaabann@gmail.com>
2018-02-22 22:07:50 +0200

    initial

---
+++ mvc1/Controllers/testController.cs
@@ -0,0 +1,31 @@
+﻿using mvc1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvc1.Controllers
+{
+    public class testController : Controller
+    {
+        // GET: test
+        // like this line
+        List<Product> products;
+        public ActionResult Index()
+        {
+            products = new List<Product>()
+            {
+                new Product {ProductId=1,Name="prod1" },
+                new Product {ProductId=2,Name="Prod2" }
+            };
+            return View(products);
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+    }
+    
+    
+}
---
+++ mvc1/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace mvc1.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+    }
+}
